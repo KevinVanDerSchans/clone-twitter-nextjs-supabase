f20494faee53ef9f8e8dca9df25d870006c4164c
Kevin Schans <kevinvdsd@hotmail.com>
2024-12-28 23:50:40 +0100

    Connection to supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies })
+  const { data: posts } = await supabase.from('posts').select()
+
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <span>HomePage</span>
+
+      <pre>{JSON.stringify(posts, null, 2)}</pre>
     </div>
   )
 }
