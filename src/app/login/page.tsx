4ae8d449253acdf46f8cb76a869e22da30b22fef
Kevin Schans <kevinvdsd@hotmail.com>
2024-12-29 16:40:20 +0100

    Adjust Login page styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,8 +2,9 @@ import { AuthButtonServer } from '@auth/components/AuthButtonServer'
 
 export default function Login() {
   return (
-    <section className='grid place-content-center min-h-screen text-center'>
-      <h1 className='text-xl font-bold mb-4 '>Welcome to Twitter Clon</h1>
+    <section className='grid place-content-center min-h-screen'>
+      <h1 className='text-xl font-bold mb-4'>Welcome to Twitter Clon</h1>
+
       <AuthButtonServer />
     </section>
   )
