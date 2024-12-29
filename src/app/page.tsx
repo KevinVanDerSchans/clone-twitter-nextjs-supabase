fb4f423d48c9994d8ca21a56e735a6ec16ff305c
Kevin Schans <kevinvdsd@hotmail.com>
2024-12-29 14:34:51 +0100

    Add Button UI to Sign out text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { redirect } from 'next/navigation'
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies })
-  const { data: posts } = await supabase.from('posts').select()
+
   const {
     data: { session },
   } = await supabase.auth.getSession()
@@ -14,11 +14,15 @@ export default async function Home() {
     redirect('/login')
   }
 
+  const { data: posts } = await supabase.from('posts').select('*')
+  const { data: users } = await supabase.from('users').select('*')
+
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <span>HomePage</span>
 
       <pre>{JSON.stringify(posts, null, 2)}</pre>
+      <pre>{JSON.stringify(users, null, 2)}</pre>
 
       <AuthButtonServer />
     </div>
