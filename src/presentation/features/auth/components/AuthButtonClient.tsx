ad2786805c2db8f484ace399bf1ea1e7082aca5d
Kevin Schans <kevinvdsd@hotmail.com>
2024-12-29 01:38:44 +0100

    Update AuthButtonClient with useAuthButtonClient hook

--- src/presentation/features/auth/components/AuthButtonClient.tsx
+++ src/presentation/features/auth/components/AuthButtonClient.tsx
@@ -1,25 +1,10 @@
 'use client'
-import { type Session, createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { useRouter } from 'next/navigation'
+import { type Session } from '@supabase/auth-helpers-nextjs'
 import { GitHubIcon } from '@constants/Icons'
+import { useAuthButtonClient } from '@auth/hooks/useAuthButtonClient'
 
 export function AuthButton({ session }: { session: Session | null }) {
-  const supabase = createClientComponentClient()
-  const router = useRouter()
-
-  const handleSignIn = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: 'github',
-      options: {
-        redirectTo: 'http://localhost:3000/auth/callback',
-      },
-    })
-  }
-
-  const handleSignOut = async () => {
-    await supabase.auth.signOut()
-    router.refresh()
-  }
+  const { handleSignIn, handleSignOut } = useAuthButtonClient()
 
   return (
     <header>
