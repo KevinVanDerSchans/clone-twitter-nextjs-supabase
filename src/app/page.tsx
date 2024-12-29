37aaf86b1691af077690aa8bafb7ec24f506f539
Kevin Schans <kevinvdsd@hotmail.com>
2024-12-29 02:00:48 +0100

    Protect home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { AuthButtonServer } from '@auth/components/AuthButtonServer'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies })
   const { data: posts } = await supabase.from('posts').select()
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if (session === null) {
+    redirect('/login')
+  }
 
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
