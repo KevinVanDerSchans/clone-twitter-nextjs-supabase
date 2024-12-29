2834c40ddf547f25ecd575b3ea602182caa020b1
Kevin Schans <kevinvdsd@hotmail.com>
2024-12-29 16:41:36 +0100

    Add PostList to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import { AuthButtonServer } from '@auth/components/AuthButtonServer'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { PostList } from 'src/presentation/features/tweets/components/PostList'
+import { AuthButtonServer } from '@auth/components/AuthButtonServer'
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies })
@@ -15,16 +16,18 @@ export default async function Home() {
   }
 
   const { data: posts } = await supabase.from('posts').select('*')
-  const { data: users } = await supabase.from('users').select('*')
+  const { data: users } = await supabase.from('users').select('id, user_name, name, avatar_url')
 
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <span>HomePage</span>
-
-      <pre>{JSON.stringify(posts, null, 2)}</pre>
-      <pre>{JSON.stringify(users, null, 2)}</pre>
+    <main className='flex min-h-screen bg-black flex-col items-center justify-between'>
+      <section className='max-w-[600px] w-full mx-auto border-l border-r border-white/20 min-h-screen'>
+        <PostList
+          posts={posts}
+          users={users}
+        />
+      </section>
 
       <AuthButtonServer />
-    </div>
+    </main>
   )
 }
