28b3933690ef6fca6a1d8d1572988657de0c4745
Kevin Schans <kevinvdsd@hotmail.com>
2024-12-29 20:14:53 +0100

    Create CreatePostForm component

---
+++ src/presentation/features/tweets/components/CreatePostForm.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { ComposePostButton } from '@tweets/components/CreatePostButton'
+import { addPost } from '@tweets/actions/AddPostAction'
+import { useRef } from 'react'
+import { Avatar } from '@nextui-org/react'
+
+export function CreatePostForm({ userAvatarUrl }: { userAvatarUrl: string }) {
+  const formRef = useRef<HTMLFormElement>(null)
+
+  return (
+    <form
+      ref={formRef}
+      action={async formData => {
+        await addPost(formData)
+        formRef.current?.reset()
+      }}
+      className='flex flex-row p-3 border-b border-white/20'
+    >
+      <div className='flex w-full gap-x-2'>
+        <div className='p-1'>
+          <Avatar
+            radius='full'
+            size='md'
+            className=''
+            src={userAvatarUrl}
+          />
+        </div>
+
+        <div className='flex flex-1 flex-col gap-y-4'>
+          <textarea
+            name='content'
+            rows={4}
+            className='w-full text-lg text-white placeholder-gray-500 p-3'
+            placeholder='What is going on?'
+          ></textarea>
+          <ComposePostButton />
+        </div>
+      </div>
+    </form>
+  )
+}
