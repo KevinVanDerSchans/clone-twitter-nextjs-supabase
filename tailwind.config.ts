6d3ece86c66bdaa8a9c732778a22547813cdc9a5
Kevin Schans <kevinvdsd@hotmail.com>
2024-12-30 00:58:31 +0100

    Improve GitHubIcon accessibility

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,11 @@
 import type { Config } from 'tailwindcss'
 import { nextui } from '@nextui-org/react'
-import themeConfig from './src/styles/themeConfig'
 
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        ...themeConfig,
         background: 'var(--background)',
         foreground: 'var(--foreground)',
       },
