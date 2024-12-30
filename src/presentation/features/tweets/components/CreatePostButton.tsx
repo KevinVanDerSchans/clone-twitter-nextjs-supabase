18dcaa85e5757fc0ee9a92c5a9f9337f660840cf
Kevin Schans <kevinvdsd@hotmail.com>
2024-12-30 01:32:34 +0100

    Adjust form

--- src/presentation/features/tweets/components/CreatePostButton.tsx
+++ src/presentation/features/tweets/components/CreatePostButton.tsx
@@ -9,6 +9,7 @@ export function ComposePostButton() {
     <button
       disabled={pending}
       type='submit'
+      aria-label='Post yout Tweet'
       className='bg-sky-600 text-sm disabled:opacity-40 disabled:pointer-events-none font-bold rounded-full px-5 py-2 self-end'
     >
       {pending ? 'Posting...' : 'Post'}
